8f463b6c290ecbd62c0b29025b264a594fcbe2ce
charles <eli.dunbar18@gmail.com>
2024-10-28 06:58:44 -0400

    completed counter program

---
+++ index.js
@@ -0,0 +1,22 @@
+// Counter program
+
+const decreaseBtn = document.getElementById("decreaseBtn");
+const resetBtn = document.getElementById("resetBtn");
+const increaseBtn = document.getElementById("increaseBtn");
+const counterLabel = document.getElementById("countLabel");
+let count = 0;
+
+increaseBtn.onclick = function() {
+    count++;
+    counterLabel.textContent = count;
+}
+
+decreaseBtn.onclick = function() {
+    count--;
+    counterLabel.textContent = count;
+}
+
+resetBtn.onclick = function() {
+    count = 0;
+    counterLabel.textContent = count;
+}
